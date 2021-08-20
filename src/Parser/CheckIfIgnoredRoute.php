4ed0ce1e41b224caa94c134f1dc12c42e7d0b89a
lara-pie <78892709+lara-pie@users.noreply.github.com>
2021-08-20 13:34:41 +0600

    Route prefixes (#6)

--- src/Parser/CheckIfIgnoredRoute.php
+++ src/Parser/CheckIfIgnoredRoute.php
@@ -4,10 +4,11 @@ namespace Larapie\Larapie\Parser;
 
 use Illuminate\Routing\Route;
 use Illuminate\Support\Str;
+use JetBrains\PhpStorm\Pure;
 
 class CheckIfIgnoredRoute
 {
-    protected $ignoredControllers = [
+    protected array $ignoredControllers = [
         'Facade\Ignition\Http\Controllers\HealthCheckController',
         'Facade\Ignition\Http\Controllers\ExecuteSolutionController',
         'Facade\Ignition\Http\Controllers\ShareReportController',
@@ -40,9 +41,94 @@ class CheckIfIgnoredRoute
         'Laravel\Telescope\Http\Controllers\HomeController',
     ];
 
+    protected array $ignoredNames = [];
+
+    protected ?array $prefixes = null;
+
+    public function __construct()
+    {
+        $routeNames = config('larapie.ignore.route-names');
+        $controllers = config('larapie.ignore.controllers');
+        $routePrefixes = config('larapie.parser.route-prefixes');
+
+        if (is_array($routeNames)) {
+            $this->ignoredNames = array_merge($this->ignoredNames, $routeNames);
+        }
+
+        if (is_array($controllers)) {
+            $this->ignoredControllers = array_merge($this->ignoredControllers, $controllers);
+        }
+
+        if (is_array($routePrefixes) && count($routePrefixes)) {
+            $this->prefixes = $routePrefixes;
+        }
+    }
+
+    /**
+     * @param  Route  $route
+     * @return bool
+     */
     public function check(Route $route): bool
     {
+        if ($this->ignoreByController($route)) {
+            return true;
+        }
+
+        if ($this->ignoreByName($route)) {
+            return true;
+        }
+
+        if ($this->ignoreByPrefix($route)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param  Route  $route
+     * @return bool
+     */
+    #[Pure] public function ignoreByPrefix(Route $route): bool
+    {
+        if (!$this->prefixes) {
+            return false;
+        }
+
+        $uri = $route->uri;
+
+        foreach ($this->prefixes as $prefix) {
+            if (Str::startsWith($uri, $prefix)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @param  Route  $route
+     * @return bool
+     */
+    public function ignoreByController(Route $route): bool
+    {
         $controller = Str::before($route->getAction('controller'), '@');
+
         return in_array($controller, $this->ignoredControllers);
+    }
+
+    /**
+     * @param  Route  $route
+     * @return bool
+     */
+    public function ignoreByName(Route $route): bool
+    {
+        $name = $route->getName();
+
+        if ($name && in_array($name, $this->ignoredNames)) {
+            return true;
+        }
+
+        return false;
     }
 }
